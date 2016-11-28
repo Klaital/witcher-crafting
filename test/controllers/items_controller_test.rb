ce96e37405265c9113eb6f733ee80c2953f2e9df
Chris Cox <chris.cox@abandonedfactory.net>
2016-11-28 14:49:01 -0800

    Made controller tests aware of login behavior

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -2,25 +2,14 @@ require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @item = items(:one)
+    @user = users(:one)
+    @item = items(:dimeritium_bomb)
   end
 
+  # The index and show actions should be public - no login required.
   test "should get index" do
     get items_url
     assert_response :success
-  end
-
-  test "should get new" do
-    get new_item_url
-    assert_response :success
-  end
-
-  test "should create item" do
-    assert_difference('Item.count') do
-      post items_url, params: { item: {  } }
-    end
-
-    assert_redirected_to item_url(Item.last)
   end
 
   test "should show item" do
@@ -28,21 +17,100 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
+  # The database-altering actions should all require a login
+
+  test "should get new when logged in" do
+    post login_path, params: {session: {email: @user.email,
+                                        password: 'foobar'}}
+    get new_item_url
+    assert_response :success
+  end
+  test "should not get new when not logged in" do
+    get new_item_url
+    assert_redirected_to login_path
+  end
+
+  test "should create item when logged in" do
+    post login_path, params: {session: {email: @user.email,
+                                        password: 'foobar'}}
+    follow_redirect!
+    assert_difference('Item.count') do
+      post items_url, params: { item: {
+          name: @item.name+' a',
+          val: @item.val,
+          slot: @item.slot,
+          tier: @item.tier,
+          craft_type: @item.craft_type,
+          armor_type: @item.armor_type,
+          weight: @item.weight,
+          thumbnail_size: @item.thumbnail_size,
+      } }
+    end
+
+    assert_redirected_to item_url(Item.last)
+  end
+
+  test "should not create item when not logged in" do
+    assert_no_difference('Item.count') do
+      post items_url, params: { item: { name: @item.name+'a', val: @item.val, slot: @item.slot, tier: @item.tier, craft_type: @item.craft_type } }
+    end
+    assert_response(401)
+  end
+
+  test "should get edit when logged in" do
+    post login_path, params: {session: {email: @user.email,
+                                        password: 'foobar'}}
     get edit_item_url(@item)
     assert_response :success
   end
 
-  test "should update item" do
-    patch item_url(@item), params: { item: {  } }
+  test "should not get edit when not logged in" do
+    get edit_item_url(@item)
+    assert_redirected_to login_path
+  end
+
+  test "should update item when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
+    patch item_url(@item), params: { item: {
+        name: @item.name+' aa',
+        val: @item.val,
+        slot: @item.slot,
+        tier: @item.tier,
+        craft_type: @item.craft_type,
+        armor_type: @item.armor_type,
+        weight: @item.weight,
+        thumbnail_size: @item.thumbnail_size,
+    } }
     assert_redirected_to item_url(@item)
   end
 
-  test "should destroy item" do
+  test "should not update item when not logged in" do
+    patch item_url(@item), params: { item: {
+        name: @item.name+' a',
+        val: @item.val,
+        slot: @item.slot,
+        tier: @item.tier,
+        craft_type: @item.craft_type,
+        armor_type: @item.armor_type,
+        weight: @item.weight,
+        thumbnail_size: @item.thumbnail_size,
+    } }
+    assert_response(401)
+  end
+
+  test "should destroy item when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
     assert_difference('Item.count', -1) do
       delete item_url(@item)
     end
 
     assert_redirected_to items_url
+  end
+
+  test "should not destroy item not when logged in" do
+    assert_no_difference('Item.count') do
+      delete item_url(@item)
+    end
+    assert_response(401)
   end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @recipe = recipes(:one)
+    @user = users(:one)
+    @recipe = recipes(:dimeritium_bomb)
   end
 
   test "should get index" do
@@ -10,39 +11,71 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
+  test "should show recipe" do
+    get recipe_url(@recipe)
+    assert_response :success
+    end
+
+  test "should get new when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
     get new_recipe_url
     assert_response :success
   end
+  test "should not get new when not logged in" do
+    get new_recipe_url
+    assert_redirected_to login_path
+  end
 
-  test "should create recipe" do
+  test "should create recipe when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
     assert_difference('Recipe.count') do
-      post recipes_url, params: { recipe: { artisan: @recipe.artisan, comments: @recipe.comments, cost: @recipe.cost, item: @recipe.item, level: @recipe.level } }
+      post recipes_url, params: { recipe: { artisan: @recipe.artisan, cost: @recipe.cost, item_id: @recipe.item_id, level: @recipe.level } }
     end
 
     assert_redirected_to recipe_url(Recipe.last)
   end
-
-  test "should show recipe" do
-    get recipe_url(@recipe)
-    assert_response :success
+  test "should not create recipe when not logged in" do
+    assert_no_difference('Recipe.count') do
+      post recipes_url, params: { recipe: { artisan: @recipe.artisan, cost: @recipe.cost, item_id: @recipe.item_id, level: @recipe.level } }
+    end
+    assert_response(401)
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
     get edit_recipe_url(@recipe)
     assert_response :success
   end
 
-  test "should update recipe" do
-    patch recipe_url(@recipe), params: { recipe: { artisan: @recipe.artisan, comments: @recipe.comments, cost: @recipe.cost, item: @recipe.item, level: @recipe.level } }
-    assert_redirected_to recipe_url(@recipe)
+  test "should not get edit when not logged in" do
+    get edit_recipe_url(@recipe)
+    assert_redirected_to login_url
   end
 
-  test "should destroy recipe" do
+  test "should update recipe when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
+    patch recipe_url(@recipe), params: { recipe: { artisan: @recipe.artisan, cost: @recipe.cost, item_id: @recipe.item_id, level: @recipe.level } }
+    assert_redirected_to recipe_url(@recipe)
+  end
+  test "should not update recipe when not logged in" do
+    patch recipe_url(@recipe), params: { recipe: { artisan: @recipe.artisan, cost: @recipe.cost, item_id: @recipe.item_id, level: @recipe.level } }
+    assert_response(401)
+  end
+
+  test "should destroy recipe when logged in" do
+    post login_path, params: {session: {email: @user.email, password: 'foobar'}}
     assert_difference('Recipe.count', -1) do
       delete recipe_url(@recipe)
     end
 
     assert_redirected_to recipes_url
+  end
+
+  test "should not destroy recipe when not logged in" do
+    assert_no_difference('Recipe.count') do
+      delete recipe_url(@recipe)
+    end
+
+    assert_response(401)
   end
 end
